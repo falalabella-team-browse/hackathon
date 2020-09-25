51382842376bc2aa8ecbd7a6144d1fd185678bad
satya <sathyaku@falabella.cl>
2020-09-25 16:33:02 +0530

    Product Page Implementation

--- website/app/pages/Shell.jsx
+++ website/app/pages/Shell.jsx
@@ -1,21 +1,11 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useUserContext } from "../context/UserContext";
-import FullScreenLoader from "../components/FullScreenLoader";
 import { Route, useHistory, useLocation } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
 import Switch from "react-bootstrap/esm/Switch";
-import Login from "../components/Login";
-import Register from "../components/Register";
-import HomePage from "./HomePage";
 import http from "../http";
+import ProductLanding from './Product/ProductLanding';
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: grid;
-  grid-template-rows: 64px 1fr;
-`;
 
 const Shell = () => {
   const [isLoaded, setLoaded] = useState(true);
@@ -52,49 +42,14 @@ const Shell = () => {
     });
   }, [history, location]);
 
-  const handleRoute = (route) => () => {
-    history.replace(route);
-  };
 
-  const handleLogout = () => {
-    logout();
-    history.replace("/login");
-  };
-
-  return (
-    <Container>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand onClick={handleRoute("/")}>
-          {process.env.APP_NAME}
-        </Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link onClick={handleRoute("/")}>Home</Nav.Link>
-        </Nav>
-        <Nav>
-          {user.isLoggedIn ? (
-            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-          ) : (
-            <Nav.Link onClick={handleRoute("/login")}>Login</Nav.Link>
-          )}
-        </Nav>
-      </Navbar>
-      {isLoaded ? (
-        <FullScreenLoader />
-      ) : (
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-        </Switch>
-      )}
-    </Container>
-  );
+  return(
+    <Switch>
+      <Route path="/" exact>
+        <ProductLanding />
+      </Route>
+    </Switch>
+  )
 };
 
 export default Shell;
