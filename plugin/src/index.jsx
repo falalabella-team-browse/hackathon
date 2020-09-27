7d450269e4c99503775fbe3e6f2eda5cbf94c9d6
Veeresh Nete <veereshnete@Veeresh-Netes-MacBook-Pro.local>
2020-09-27 14:38:29 +0530

    pdp base url updated

--- plugin/src/index.jsx
+++ plugin/src/index.jsx
@@ -1,44 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import RootContainer from "./containers/Root/Root";
-import UserContainer from "./containers/UserContainer";
-import UserContext from "./Context/UserContext";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RootContainer from './containers/Root/Root';
+import UserContainer from './containers/UserContainer';
+import UserContext from './Context/UserContext';
 
 const load = (element, user) => {
-  ReactDOM.render(
-    <UserContext.Provider value={user}>
-      <RootContainer />
-    </UserContext.Provider>,
-    element
-  );
+	ReactDOM.render(
+		<UserContext.Provider value={user}>
+			<RootContainer />
+		</UserContext.Provider>,
+		element
+	);
 };
 
 const loadUserPanel = (element, user) => {
-  ReactDOM.render(
-    <UserContext.Provider value={user}>
-      <UserContainer />
-    </UserContext.Provider>,
-    element
-  );
+	ReactDOM.render(
+		<UserContext.Provider value={user}>
+			<UserContainer />
+		</UserContext.Provider>,
+		element
+	);
 };
 
-if (process.env.NODE_ENV !== "production") {
-  window.RNR = {
-    basePath: "https://hack.rlab.app",
-    // basePath: "http://localhost:3000",
-  };
+if (process.env.NODE_ENV !== 'production') {
+	window.RNR = {
+		basePath: 'https://hackathon.rlab.app',
+	};
 
-  loadUserPanel(document.getElementById("reviews"), {
-    userId: "12",
-    productId: "929",
-  });
+	loadUserPanel(document.getElementById('reviews'), {
+		userId: '12',
+		productId: '929',
+	});
 } else {
-  window.RNR = {
-    load,
-    loadUserPanel,
-    basePath: "http://localhost:3000",
-    setHost: (path) => {
-      window.RNR.basePath = path;
-    },
-  };
+	window.RNR = {
+		load,
+		loadUserPanel,
+		basePath: 'https://hackathon.rlab.app',
+		setHost: path => {
+			window.RNR.basePath = path;
+		},
+	};
 }
